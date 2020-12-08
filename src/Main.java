12eabedd84506b85ec195ec37c9aeadf42c68c5e
Alex <plumbum531@gmail.com>
2020-12-08 20:58:17 +0200

    Pow function for floats is implemented.
    * The function is not static.
    * Negativ pow is not handled.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        
+
+    }
+
+    public static float pow(float value, int pow){
+        float result = 1;
+
+        for(int i =0; i< pow; i++){
+            result *=value;
+        }
+        return result;
     }
 }
